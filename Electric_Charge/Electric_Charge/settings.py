9581bda4ff64ba03ee71ef9621eb19e3a07eb815
afsana1210 <afsana1210@gmail.com>
2020-08-24 16:48:55 +0530

    added django app into settings.py

--- Electric_Charge/Electric_Charge/settings.py
+++ Electric_Charge/Electric_Charge/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['localhost']
 # Application definition
 
 INSTALLED_APPS = [
+    'ECP.apps.EcpConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
