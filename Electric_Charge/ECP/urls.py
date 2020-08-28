f9fb5462ce88f936e143eda61c8f1abd4b668f98
afsana1210 <afsana1210@gmail.com>
2020-08-28 13:59:08 +0530

    added new templates

--- Electric_Charge/ECP/urls.py
+++ Electric_Charge/ECP/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from ECP import views
-from .views import signup_view,list_view
+from .views import signup_view,list_view,signin_view,signin_view_work
 
 urlpatterns=[
     # path('',views.index,name='index'),
@@ -9,4 +9,6 @@ urlpatterns=[
 
     path('signup/',views.signup_view ,name='signup'),
     path('list/',views.list_view ,name='list'),
+    path('signin/',views.signin_view,name='signin'),
+    path('signin_work/',views.signin_view_work,name='signin')
 ]
--- Electric_Charge/ECP/views.py
+++ Electric_Charge/ECP/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 from .models import Signup
 from .forms import SignupModelForm
 
@@ -22,8 +22,61 @@ def signup_view(request):
 
 def list_view(request):
     # email=self.cleaned_data.get('email')
-    qs=Signup.objects.get(email=request.POST['email'])
-    print("query :",qs)
+    qs=Signup.objects.all()
+    print("object_list :",qs)
     template_name='ECP/list.html'
     context={'object_list':qs}
     return render(request,template_name,context)
+
+def signin_view(request):
+    if request.method=="POST":
+        data=request.POST.get("email_id")
+        # data=SignupModelForm(request.POST)
+        # print(data)
+        qs=Signup.objects.all()
+    # context={"object_list":qs}
+    # if request.method=='POST':
+        for objects in qs:
+            email=objects.email
+            print("email is :" ,email)
+        print("data :",data)
+        if email==data:
+              print("email :",email) 
+              return render(request,"ECP/list.html")
+        print("email of signin:",email)
+       
+    template_name='ECP/signin.html'
+    return render(request,template_name)
+   
+    # context={'user':qs.user,'email':qs.email,'password':qs.password}
+    # return render(request,template_name)
+   
+    
+    
+    # if qs.user==1 and qs.email==request.POST['email'] and qs.password==request.POST['password']:
+    #     return render(request,"ECP/list.html")
+    # return render(request,"not right")
+
+def signin_view_work(request):
+    if request.method=="POST":
+        data=request.POST.get("email_id")
+        data1=request.POST.get("password")
+        qs=Signup.objects.all()
+        for objects in qs:
+            email=objects.email
+            password=objects.password
+            print("email is :" ,email)
+            print("password is:",password)
+            print("data :",data)
+            print("data 1 is :",data1)
+            if email==data and password==data1:
+              print("email :",email)
+              print("password :",password) 
+              return render(request,"ECP/list.html")
+            # else:
+                # return HttpResponse("please check your email and password.you're email and password didn't match ","ECP/signin.html")
+        print("email of signin:",email)
+        print("password of signin :" , password)
+       
+    template_name='ECP/signin.html'
+    return render(request,template_name)
